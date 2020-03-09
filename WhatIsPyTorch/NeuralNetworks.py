f0ff65c2f00c6d5199e6443bdd591b9030c4fb24
Ninebell <ninebell94@gmail.com>
2020-03-09 17:19:55 +0900

    about NeuralNetworks

---
+++ WhatIsPyTorch/NeuralNetworks.py
@@ -0,0 +1,82 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        # 1 input image channel, 6 output channels, 3x3 square convolution
+        # kernel
+        self.conv1 = nn.Conv2d(1, 6, 3)
+        self.conv2 = nn.Conv2d(6, 16, 3)
+        # an affine operation: y = Wx + b
+        self.fc1 = nn.Linear(16*6*6, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        # Max pooling over a (2, 2) window
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+        # If the size is a square you can only specify a single number
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = x.view(-1, self.num_flat_features(x))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+    def num_flat_features(self, x):
+        size = x.size()[1:]
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
+
+
+net = Net()
+print(net)
+
+params = list(net.parameters())
+print(len(params))
+print(params[0].size())
+# pytorch only supports mini-batch sample. if you use single sample, you need to use input.unsqueeze(0)
+input = torch.randn(1, 1,32,32)
+
+out = net(input)
+print(out)
+
+net.zero_grad()
+out.backward(torch.randn(1, 10))
+
+output = net(input)
+
+target = torch.randn(10) # a ddummy target, for example
+target= target.view(1, -1) # make i the same shape as output
+criterion = nn.MSELoss()
+loss = criterion(output, target)
+print(loss)
+
+print(loss.grad_fn)
+print(loss.grad_fn.next_functions[0][0])
+print(loss.grad_fn.next_functions[0][0].next_functions[0][0])
+
+net.zero_grad()
+
+print('conv1.bias.grad before backward')
+print(net.conv1.bias.grad)
+loss.backward()
+print('conv1.bias.grad after backward')
+print(net.conv1.bias.grad)
+
+# how to use optimizer
+import torch.optim as optim
+optimizer = optim.SGD(net.parameters(), lr=0.01)
+
+optimizer.zero_grad()
+output = net(input)
+
+loss = criterion(output,target)
+loss.backward()
+optimizer.step()
+
